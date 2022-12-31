7cdd1b76c0cc483a5136b750a38d52140ba581cb
jamellknows <jamellsamuels@googlemail.com>
2022-12-31 07:37:39 +0000

    my first push

---
+++ index.js
@@ -0,0 +1,52 @@
+let textarea = document.getElementById("textarea1") 
+
+function f1() {
+    textarea.style.fontWeight = "bold"
+}
+
+function f2() {
+    textarea.style.fontStyle = "italic"
+}
+
+function f3() {
+    textarea.style.textAlign = "left"
+}
+
+
+function f4() {
+    textarea.style.textAlign = "center"
+}
+
+
+function f5() {
+    textarea.style.textAlign = "right"
+}
+
+
+function f6() {
+    textarea.style.textTransform = "uppercase"
+}
+
+
+function f7() {
+    textarea.style.textTransform = "lowercase"
+}
+
+
+function f8() {
+    textarea.style.textTransform = "capitalise"
+}
+
+function f9() {
+    textarea.style.fontWeight = "normal"
+    textarea.style.textAlign = "left"
+    textarea.style.fontStyle = "normal"
+    textarea.style.textTransform = "capitalize"
+    textarea.value = " "
+    
+}
+
+function save(){
+
+}
+
